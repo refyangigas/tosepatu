f0154ce2bb199dfceab040178d0f17665e2236d2
refyangigas <119940877+refyangigas@users.noreply.github.com>
2022-12-14 17:46:17 +0700

    BELAJAR

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Login</title>
+    <h1>BARU BELAJAR</h1>
 </head>
 <body>
 <!-- <a href="https://front.codes/" class="logo" target="_blank">
@@ -27,7 +28,6 @@
 								<div class="card-front">
                                 <form action='login.php' method="POST" >
 									<div class="center-wrap">
-										
                                             <div class="section text-center">
 											<h4 class="mb-4 pb-3">Log In</h4>
 											<div class="form-group">
