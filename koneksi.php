18a0f662d0152b32532f5034a0259f5bc2a58c4e
lanaibnufauzi <120556006+lanaibnufauzi@users.noreply.github.com>
2022-12-15 19:37:49 +0700

    add form login

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$db ="tosepatu";
-
-$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-?>
