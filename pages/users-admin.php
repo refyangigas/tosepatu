0e7b5b6e47e5405529e9002d63583c5fd51c622b
refyangigas <119940877+refyangigas@users.noreply.github.com>
2022-12-31 13:55:28 +0700

    Update users-admin.php

--- pages/users-admin.php
+++ pages/users-admin.php
@@ -1,3 +1,22 @@
+<?php
+include "../connection/koneksi.php";
+session_start();
+
+if (isset($_SESSION["ses_username"]) == "") {
+  header("location: ../login.php");
+} elseif ($_SESSION["ses_role"] == '2') {
+  header("location: logout.php");
+} else {
+  $data_id = $_SESSION["ses_id"];
+  $data_nama = $_SESSION["ses_nama"];
+  $data_username = $_SESSION["ses_username"];
+  $data_password = $_SESSION["ses_password"];
+  $data_role = $_SESSION["ses_role"];
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
