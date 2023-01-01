ca7596a8ca1ad3016a7b693afe69e5b0251d5a77
lanaibnufauzi <120556006+lanaibnufauzi@users.noreply.github.com>
2023-01-01 14:11:37 +0700

    add lanjutan html orders

--- pages/ordres.php
+++ pages/ordres.php
@@ -71,3 +71,128 @@
 
       </ul>
     </div>
+
+    
+  </aside>
+  <main class="main-content position-relative border-radius-lg">
+    <!-- Navbar -->
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="false">
+      <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm">
+              <a class="opacity-5 text-white" href="javascript:;">Pages</a>
+            </li>
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page">
+              Orders
+            </li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Orders</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          <!-- <div class="ms-md-auto pe-md-3 d-flex align-items-center"> -->
+          <!-- <div class="input-group">
+              <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+              <input type="text" class="form-control" placeholder="Type here..." />
+            </div> -->
+          <!-- </div> -->
+          <ul class="ms-md-auto pe-md-3 d-flex align-items-center navbar-nav justify-content-end">
+            <!-- <li class="nav-item d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+                <i class="fa fa-user me-sm-1"></i>
+                <span class="d-sm-inline d-none">Sign In</span>
+              </a>
+            </li> -->
+            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                <div class="sidenav-toggler-inner">
+                  <i class="sidenav-toggler-line bg-white"></i>
+                  <i class="sidenav-toggler-line bg-white"></i>
+                  <i class="sidenav-toggler-line bg-white"></i>
+                </div>
+              </a>
+            </li>
+
+
+            <?php
+            error_reporting(0);
+
+
+            $tampilprofil = ("SELECT * FROM tb_user WHERE id = '$data_id'");
+            $result   = mysqli_query($koneksi, $tampilprofil);
+
+            while ($row = mysqli_fetch_array($result)) {
+
+              $profilName   = $row['nama'];
+
+            ?>
+
+              <li class="nav-item px-3 d-flex align-items-center">
+                <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+                  <span class="d-sm-inline d-none">Halo, <?php echo $profilName ?></span>
+                </a>
+              </li>
+
+            <?php
+
+            }
+            ?>
+            <li class="nav-item dropdown pe-2 d-flex align-items-center">
+              <a href="#" class="nav-link text-white p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fa fa-chevron-circle-down cursor-pointer"></i>
+              </a>
+
+              <ul class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
+
+                <li class="dropdown-item d-flex align-items-center">
+                  <a href="profile.php" class="dropdown-item">
+                    <i class="fa fa-user fixed-plugin-button-nav cursor-pointer"></i> Profile
+                  </a>
+
+                </li>
+                <li class="dropdown-item d-flex align-items-center">
+                  <a href="logout.php" class="dropdown-item">
+                    <i class="fa fa-sign-out cursor-pointer"></i> Logout
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- End Navbar -->
+
+    <div class="card shadow-lg mx-4 mt-3">
+      <div class="card-body">
+        <div class="row gx-4">
+
+          <!-- <div class="dropdown col-auto">
+            <form class="" action="" method="post">
+              <button class="btn btn-sm bg-gradient-dark dropdown-toggle mb-1 px-3" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Sort By
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <button class="dropdown-item" name="namaasc" type="submit"> Nama Asc (A-Z)</button>
+                <button class="dropdown-item" name="namadesc" type="submit"> Nama Desc (Z-A)</button>
+                <button class="dropdown-item" name="totalasc" type="submit">Total Desc (A-Z)</button>
+                <button class="dropdown-item" name="totaldesc" type="submit">Total Asc (Z-A)</button>
+              </ul>
+            </form>
+
+          </div> -->
+
+          <div class="dropdown col-auto">
+            <form class="" action="" method="post">
+              <button class="btn btn-sm bg-gradient-dark dropdown-toggle mb-1 px-3" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Status
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <button class="dropdown-item" name="belum-bayar" type="submit">Belum Bayar</button>
+                <button class="dropdown-item" name="pengerjaan" type="submit">Pengerjaan</button>
+                <button class="dropdown-item" name="selesai" type="submit">Selesai</button>
+                <button class="dropdown-item" name="gagal" type="submit">Gagal</button>
+              </ul>
+            </form>
+
+          </div>
