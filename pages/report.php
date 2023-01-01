d289783722488cdfcec1105320b03ab818dab02a
refyangigas <119940877+refyangigas@users.noreply.github.com>
2023-01-01 13:48:55 +0700

    Create report.php

---
+++ pages/report.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+  <title>Report - Suki Dashboard</title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+
+  <link id="pagestyle" href="../assets/css/argon-dashboard.css" rel="stylesheet" />
+</head>
+
+<body class="g-sidenav-show bg-gray-100">
+  <div class="position-absolute w-100 min-height-300 top-0" style="
+        background-image: url('../assets/img/nv-bg.jpg');
+        background-position-y: 50%;
+      ">
+    <span class="mask bg-primary opacity-6"></span>
+  </div>
+  <div class="min-height-300 position-absolute w-100"></div>
+  <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4" id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href="./dashboard.php">
+        <img src="../assets/images/icon.png" class="navbar-brand-img h-100" alt="main_logo" />
+        <span class="ms-1 font-weight-bold">Tosepatu Dashboard</span>
+      </a>
+    </div>
+    <hr class="horizontal dark mt-0" />
+    <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="./dashboard.php">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-tv-2 text-warning text-sm opacity-10"></i>
+              <!-- <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i> -->
+            </div>
+            <span class="nav-link-text ms-1">Dashboard</span>
+          </a>
+        </li>
+
+
+        <li class='nav-item'>
+          <a class='nav-link' href='transaksi.php'>
+            <div class='icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center'>
+              <i class='ni ni-tag text-warning text-sm opacity-10'></i>
+            </div>
+            <span class='nav-link-text ms-1'>Transaksi</span>
+          </a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link' href='services.php'>
+            <div class='icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center'>
+              <i class='ni ni-bulb-61 text-warning text-sm opacity-10'></i>
+            </div>
+            <span class='nav-link-text ms-1'>Services</span>
+          </a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link' href='users.php'>
+            <div class='icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center'>
+              <i class='ni ni-single-02 text-warning text-sm opacity-10'></i>
+            </div>
+            <span class='nav-link-text ms-1'>Users</span>
+          </a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link' href='messages.php'>
+            <div class='icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center'>
+              <i class='ni ni-send text-warning text-sm opacity-10'></i>
+            </div>
+            <span class='nav-link-text ms-1'>Messages</span>
+          </a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link active' href='report.php'>
+            <div class='icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center'>
+              <i class='ni ni-chart-bar-32 text-warning text-sm opacity-10'></i>
+            </div>
+            <span class='nav-link-text ms-1'>Report</span>
+          </a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link' href='profile.php'>
+            <div class='icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center'>
+              <i class='ni ni-circle-08 text-warning text-sm opacity-10'></i>
+            </div>
+            <span class='nav-link-text ms-1'>Profile</span>
+          </a>
+        </li>
+
+      </ul>
+    </div>
+
+
+  </aside>
+  <main class="main-content position-relative border-radius-lg">
+    <!-- Navbar -->
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="false">
+      <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm">
+              <a class="opacity-5 text-white" href="javascript:;">Pages</a>
+            </li>
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page">
+              Report
+            </li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Report</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          <!-- <div class="ms-md-auto pe-md-3 d-flex align-items-center"> -->
+          <!-- <div class="input-group">
+              <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+              <input type="text" class="form-control" placeholder="Type here..." />
+            </div> -->
+          <!-- </div> -->
+          <ul class="ms-md-auto pe-md-3 d-flex align-items-center navbar-nav justify-content-end">
+            <!-- <li class="nav-item d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+                <i class="fa fa-user me-sm-1"></i>
+                <span class="d-sm-inline d-none">Sign In</span>
+              </a>
+            </li> -->
+            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                <div class="sidenav-toggler-inner">
+                  <i class="sidenav-toggler-line bg-white"></i>
+                  <i class="sidenav-toggler-line bg-white"></i>
+                  <i class="sidenav-toggler-line bg-white"></i>
+                </div>
+              </a>
+            </li>
