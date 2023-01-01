0f41538f8bb0cdcca6c895d23bc6de6490b5ba0e
refyangigas <119940877+refyangigas@users.noreply.github.com>
2023-01-01 14:00:25 +0700

    Update report.php

--- pages/report.php
+++ pages/report.php
@@ -350,3 +350,141 @@ if (isset($_SESSION["ses_username"]) == "") {
 
 
                 ?>
+
+                <?php
+
+                if ($JumlahTransaksi == '') {
+                echo "
+                <h6 class= 'ms-auto font-weight-bolder'>Jumlah Transaksi : 0 </h6>&nbsp;&nbsp;&nbsp;&nbsp;
+                ";
+                } else {
+                echo "
+                <h6 class= 'ms-auto font-weight-bolder'>Jumlah Transaksi : $JumlahTransaksi </h6>&nbsp;&nbsp;&nbsp;&nbsp;
+                ";
+                }
+
+                if ($TotalJumlah == '') {
+
+                echo "
+
+                <h6 class='ms font-weight-bolder'>Total : 0 </h6>
+                ";
+                } else {
+
+                echo "
+
+                <h6 class='ms font-weight-bolder'>Total :Rp. $TotalJumlah </h6>
+                ";
+                }
+
+                ?>
+
+                <?php
+
+                }
+
+                ?>
+
+            </div>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        No
+                      </th>
+
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        Status
+                      </th>
+
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        Nama
+                      </th>
+
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        Layanan
+                      </th>
+
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        Jumlah
+                      </th>
+
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        Pembayaran
+                      </th>
+
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        Total
+                      </th>
+
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                        Tanggal
+                      </th>
+
+                    </tr>
+                  </thead>
+                  <tbody>
+
+                  <footer class="footer pt-3">
+        <div class="container-fluid">
+          <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+              <div class="copyright text-center text-sm text-muted text-lg-start">
+                ©
+                <script>
+                  document.write(new Date().getFullYear());
+                </script>
+                , made with <i class="fa fa-heart"></i> by
+                <a href="#" class="font-weight-bold">Tosepatu Team</a>
+                for a better web.
+              </div>
+            </div>
+            <!-- <div class="col-lg-6">
+              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                </li>
+              </ul>
+            </div> -->
+          </div>
+        </div>
+      </footer>
+    </div>
+  </main>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf("Win") > -1;
+    if (win && document.querySelector("#sidenav-scrollbar")) {
+      var options = {
+        damping: "0.5",
+      };
+      Scrollbar.init(document.querySelector("#sidenav-scrollbar"), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/argon-dashboard.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+</body>
+
+</html>
+
+                  
