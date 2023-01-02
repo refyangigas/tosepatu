a15168865f033d63124bea622e8cdee75ab2bd20
refyangigas <119940877+refyangigas@users.noreply.github.com>
2023-01-02 07:53:56 +0700

    finishing

--- pages/messages-users.php
+++ pages/messages-users.php
@@ -23,7 +23,7 @@ if (isset($_SESSION["ses_username"]) == "") {
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
   <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
-  <title>Messages Users - Suki Dashboard</title>
+  <title>Messages Users - Tosepatu Dashboard</title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
